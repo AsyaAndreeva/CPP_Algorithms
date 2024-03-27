46c36e2330aabc83f4b51b1620208ab01b5b623a
Asya Andreeva <117649237+AsyaAndreeva@users.noreply.github.com>
2024-03-27 17:03:11 +0200

    Added insertion sort

--- Insertion Sort/cmake-build-debug/cmake_install.cmake
+++ Insertion Sort/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: G:/Asya/CPP_Algorithums/Insertion Sort
+# Install script for directory: G:/Asya/CPP_Algorithms/Insertion Sort
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "G:/Asya/CPP_Algorithums/Insertion Sort/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "G:/Asya/CPP_Algorithms/Insertion Sort/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Insertion Sort/main.cpp
+++ Insertion Sort/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+using namespace std;
+
+void insertionSort(int* arrayInput, int size);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int arr[] = {12, 11, 13, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Array before sorting: ";
+    for(int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    insertionSort(arr, size);
+
+    cout << "Array after sorting: ";
+    for(int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
     return 0;
+}
+
+void insertionSort(int* arrayInput, int size){
+    for(int j = 1; j < size; j++){
+        int key = arrayInput[j];
+        int i = j - 1;
+        while(i >= 0 && arrayInput[i] > key){
+            arrayInput[i + 1] = arrayInput[i];
+            i--;
+        }
+        arrayInput[i + 1] = key;
+    }
 }
